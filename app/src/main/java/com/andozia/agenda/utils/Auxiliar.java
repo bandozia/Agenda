bc9724377c69905fb8802d037e96d5441f739b73
bandozia <bruno.andozia@gmail.com>
2017-07-24 22:59:33 -0300

    download async incluido

---
+++ app/src/main/java/com/andozia/agenda/utils/Auxiliar.java
@@ -0,0 +1,30 @@
+package com.andozia.agenda.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+/**
+ * Created by no3603 on 24/07/2017.
+ */
+
+public class Auxiliar {
+
+    public static Bitmap baixarImagem(String url) throws IOException{
+        URL endereco;
+        InputStream inputStream;
+        Bitmap imagem;
+
+        endereco = new URL(url);
+        inputStream = endereco.openStream();
+        imagem = BitmapFactory.decodeStream(inputStream);
+
+        inputStream.close();
+
+        return imagem;
+    }
+
+}
